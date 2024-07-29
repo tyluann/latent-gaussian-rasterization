33d14e77861ed99652c5b820792e6e0697e1bfe2
tyluann <13264062239@163.com>
2024-07-29 04:17:46 +0000

    Update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git add -A
 git commit -m "Update"
 git push
-pip uninstall diff-gaussian-rasterization-test
+pip uninstall diff-gaussian-rasterization-test -y
 pip install git+https://github.com/tyluann/latent-gaussian-rasterization.git
